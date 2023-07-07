7ad7b5b8a8ddda8e3ca239c1a02efe60bdd8b822
Yuval Kashtan <yuvalkashtan@gmail.com>
2023-07-07 18:17:26 +0300

    introduce new ovn-conf-ipsec
    this add ipsec extension to ipsec step
    to be used for >=4.14,
    where ovn IPsec moved the host and is dependent on that extension in
    RHCOS

---
+++ ci-operator/step-registry/ovn/conf/ipsec-manifest/ovn-conf-ipsec-manifest-commands.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+cat >> "${SHARED_DIR}/manifest_cluster-network-03-config.yml" << EOF
+apiVersion: operator.openshift.io/v1
+kind: Network
+metadata:
+  creationTimestamp: null
+  name: cluster
+spec:
+  clusterNetwork:
+  - cidr: 10.128.0.0/14
+    hostPrefix: 23
+  externalIP:
+    policy: {}
+  networkType: OVNKubernetes
+  serviceNetwork:
+  - 172.30.0.0/16
+  defaultNetwork:
+    type: OVNKubernetes
+    ovnKubernetesConfig:
+      ipsecConfig: {}
+EOF
+
+for role in master worker; do
+cat >> "${SHARED_DIR}/manifest_${role}-ipsec-extension.yml" << EOF
+apiVersion: machineconfiguration.openshift.io/v1
+kind: MachineConfig
+metadata:
+  labels:
+    machineconfiguration.openshift.io/role: $role
+  name: 80-$role-extensions
+spec:
+  config:
+    ignition:
+      version: 3.2.0
+  extensions:
+    - ipsec
+EOF
+done
